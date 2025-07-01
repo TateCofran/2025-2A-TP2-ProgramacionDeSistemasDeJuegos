2d284e2bce0a9a3c30c63a2a4e1d2ca515099fe1
TateCofran <drogofranco@outlook.com>
2025-07-01 01:52:20 -0300

    Added buttons

---
+++ Assets/Scripts/Buttons/ButtonsPrefabConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ButtonsPrefab", menuName = "Scriptable Objects/ButtonsPrefab")]
+public class ButtonsPrefabConfig : ScriptableObject
+{
+    public ButtonPrefab[] buttons;
+
+    [Serializable]
+    public class ButtonPrefab
+    {
+        public string bttnTextTittle;
+        public CharacterPrefabConfig characterPrefab;
+    }
+
+}
