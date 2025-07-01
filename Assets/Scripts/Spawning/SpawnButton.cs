c4dc9cb9ba86ba3fa35b756f1aea87b0a4dc0789
TateCofran <drogofranco@outlook.com>
2025-07-01 01:43:11 -0300

    CharacterPrefabs

--- Assets/Scripts/Spawning/SpawnButton.cs
+++ Assets/Scripts/Spawning/SpawnButton.cs
@@ -34,6 +34,6 @@ public class SpawnButton : MonoBehaviour
     private void HandleClick()
     {
         var spawner = FindFirstObjectByType<CharacterSpawner>();
-        spawner.Spawn();
+        //spawner.Spawn();
     }
 }
